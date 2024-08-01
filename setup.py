d8fa6a038838cce893d5254ffcbfe850567a9efc
xiongxinlei <915574300@qq.com>
2024-08-01 10:47:25 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+long_description = "A Python library for tiktoken API"
+setup(name="tiktoken-py",
+      version=0.1,
+      description=long_description,
+      url="https://github.com/TikTokAPI/tiktoken-py",
+      author="xiongxinlei",
+      author_email="915574300@qq.com",
+      license="MIT",
+      packages=["tiktoken_py"],
+      zip_safe=False)
